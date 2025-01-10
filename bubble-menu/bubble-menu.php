bb4110ec1dd9c2fc527b18cf7f3e252915fe91c6
Sean Murphy <sean@iamseanmurphy.com>
2025-01-10 13:20:13 +0000

    4.0.3

--- bubble-menu/bubble-menu.php
+++ bubble-menu/bubble-menu.php
@@ -3,7 +3,7 @@
  *  Plugin Name:       Bubble Menu
  *  Plugin URI:        https://wordpress.org/plugin/bubble-menu
  *  Description:       Creating awesome circle menu with icons.
- *  Version:           4.0.2
+ *  Version:           4.0.3
  *  Author:            Wow-Company
  *  Author URI:        https://wow-estore.com/
  *  License:           GPL-2.0+
